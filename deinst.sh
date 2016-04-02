828bf95e5b56101331f73050b77f92e22a3d79c4
aquila62 <orvalbounds@yahoo.com>
2016-04-02 04:39:30 -0500

    Added the Poisson Distribution Chi Square Test

--- deinst.sh
+++ deinst.sh
@@ -3,6 +3,7 @@ make -f tsteegl.mak clean
 make -f tstnc.mak clean
 make -f tstdots.mak clean
 make -f eeglchi.mak clean
+make -f poischi.mak clean
 make -f binom.mak clean
 make -f eeglgen.mak clean
 make -f eeglraw.mak clean
